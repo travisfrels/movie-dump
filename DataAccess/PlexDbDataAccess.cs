eadd1a67f8ce2e46fd3573aa71058ead22f8ff89
Travis Frels <travis.frels@gmail.com>
2019-11-15 12:21:48 -0600

    updated from khousie

---
+++ DataAccess/PlexDbDataAccess.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.Data.SQLite;
+using Dapper;
+using MovieDump.Models;
+
+namespace MovieDump.DataAccess
+{
+    public class PlexDbDataAccess
+    {
+        private const string moviesQuery = @"
+SELECT
+    meta.title AS [Title],
+    meta.year AS [Year],
+    CASE media.height WHEN 480 THEN 'DVD' WHEN 2160 THEN '4K' ELSE 'BLU-RAY' END AS [Format],
+    media.duration AS [Duration],
+    media.video_codec as [VideoCodec],
+    media.audio_codec as [AudioCodec]
+FROM media_items AS media
+    INNER JOIN library_sections AS lib ON media.library_section_id = lib.id
+    INNER JOIN metadata_items AS meta ON media.metadata_item_id = meta.id
+    LEFT OUTER JOIN media_parts AS mp ON media.id = mp.media_item_id
+WHERE meta.parent_id IS NULL
+ORDER BY meta.title, meta.year, media.video_codec, media.audio_codec
+";
+
+        private readonly string _connString;
+
+        public PlexDbDataAccess(string connString)
+        {
+            this._connString = connString;
+        }
+
+        public IEnumerable<Movie> GetMovies()
+        {
+            using (var conn = new SQLiteConnection(_connString))
+            {
+                conn.Open();
+                return conn.Query<Movie>(moviesQuery);
+            }
+        }
+    }
+}
--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,9 +1,16 @@
+using System;
+
 namespace MovieDump.Models
 {
     public class Movie
     {
         public string Title { get; set; }
-        public int Size { get; set; }
+        public string Year { get; set; }
         public string Format { get; set; }
+
+        private TimeSpan _duration;
+        public string Duration { get { return _duration.ToString(@"hh\:mm\:ss"); } set { _duration = TimeSpan.FromMilliseconds(int.Parse(value)); } }
+        public string VideoCodec { get; set; }
+        public string AudioCodec { get; set; }
     }
 }
